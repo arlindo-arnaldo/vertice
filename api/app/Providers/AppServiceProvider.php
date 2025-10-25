b77b99cf193544615a01ef8c704161cca06c9805
arlindo-arnaldo <arlindoarnaldocassuende@gmail.com>
2025-10-25 07:30:55 +0100

    Configurando personal access token

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
+use App\Models\Sanctum\PersonalAccessToken;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/make-token', function () {
+    $user = User::first();
+    $token = $user->createToken('dev-token')->plainTextToken;
+    return ['token' => $token];
+})->name('login');
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
